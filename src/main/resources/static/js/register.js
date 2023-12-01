074d7a753d5e41568fd14c486dd0dec777fa98e2
AndresOnate <andres.onate@mail.escuelaing.edu.co>
2023-12-01 14:54:15 -0500

    BackendPool

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -13,7 +13,7 @@ var register = (function () {
 
     var connectAndSubscribe = function () {
         console.info('Connecting to WS...');
-        var socket = new SockJS('typefightbackendpool.eastus.cloudapp.azure.com/stompendpoint');
+        var socket = new SockJS('http://typefightbackendpool.eastus.cloudapp.azure.com/stompendpoint');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, function (frame) {
